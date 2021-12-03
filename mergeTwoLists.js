882eb6a31fc490f04be31d04158a6e2d95ac3d47
leslieyi <leslieyi411@gmail.com>
2021-12-03 15:04:32 -0500

    review later

---
+++ mergeTwoLists.js
@@ -0,0 +1,47 @@
+// 21. Merge Two Sorted Lists
+// You are given the heads of two sorted linked lists list1 and list2.
+
+// Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+// Return the head of the merged linked list.
+
+// Example 1:
+// Input: list1 = [1,2,4], list2 = [1,3,4]
+// Output: [1,1,2,3,4,4]
+
+// Example 2:
+
+// Input: list1 = [], list2 = []
+// Output: []
+
+// Example 3:
+
+// Input: list1 = [], list2 = [0]
+// Output: [0]
+
+// Constraints:
+
+// The number of nodes in both lists is in the range [0, 50].
+// -100 <= Node.val <= 100
+// Both list1 and list2 are sorted in non-decreasing order.
+
+function mergeTwoLists(list1, list2) {
+  let merge = new ListNode();
+  let head = merge;
+
+  while (l1 && l2) {
+    if (l1.val < l2.val) {
+      merge.next = l1;
+      l1 = l1.next;
+    } else {
+      merge.next = l2;
+      l2 = l2.next;
+    }
+    merge = merge.next;
+  }
+
+  if (!l1) merge.next = l2;
+  else if (!l2) merge.next = l1;
+
+  return head.next; // why do this? because
+}
